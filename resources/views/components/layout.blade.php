a13fce840019449e3dd0036ecf6378bf96358bf4
Oleg Alexandrov <alexandrovoleg.ru@gmail.com>
2025-02-28 14:22:52 +0200

    Lesson 5

--- resources/views/components/layout.blade.php
+++ resources/views/components/layout.blade.php
@@ -1,19 +1,12 @@
-<html>
+<!doctype html>
+<html class="h-full bg-gray-100">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
     <script src="https://cdn.tailwindcss.com"></script>
 </head>
-<body>
-<!--
-  This example requires updating your template:
-
-  ```
-  <html class="h-full bg-gray-100">
-  <body class="h-full">
-  ```
--->
+<body class="h-full">
 <div class="min-h-full">
     <nav class="bg-gray-800">
         <div class="mx-auto max-w-7xl px-4 sm:px-6 lg:px-8">
@@ -26,13 +19,9 @@
                     </div>
                     <div class="hidden md:block">
                         <div class="ml-10 flex items-baseline space-x-4">
-                            <!-- Current: "bg-gray-900 text-white", Default: "text-gray-300 hover:bg-gray-700 hover:text-white" -->
-                            <a href="/" class="rounded-md bg-gray-900 px-3 py-2 text-sm font-medium text-white"
-                               aria-current="page">Home</a>
-                            <a href="/about"
-                               class="rounded-md px-3 py-2 text-sm font-medium text-gray-300 hover:bg-gray-700 hover:text-white">About</a>
-                            <a href="/contact"
-                               class="rounded-md px-3 py-2 text-sm font-medium text-gray-300 hover:bg-gray-700 hover:text-white">Contact</a>
+                            <x-nav-link href="/" :active="request()->is('/')">Home</x-nav-link>
+                            <x-nav-link href="/about" :active="request()->is('about')">About</x-nav-link>
+                            <x-nav-link href="/contact" :active="request()->is('contact')">Contact</x-nav-link>
                         </div>
                     </div>
                 </div>
@@ -42,7 +31,8 @@
                                 class="relative rounded-full bg-gray-800 p-1 text-gray-400 hover:text-white focus:ring-2 focus:ring-white focus:ring-offset-2 focus:ring-offset-gray-800 focus:outline-hidden">
                             <span class="absolute -inset-1.5"></span>
                             <span class="sr-only">View notifications</span>
-                            <svg class="size-6" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor"
+                            <svg class="size-6" fill="none" viewBox="0 0 24 24" stroke-width="1.5"
+                                 stroke="currentColor"
                                  aria-hidden="true" data-slot="icon">
                                 <path stroke-linecap="round" stroke-linejoin="round"
                                       d="M14.857 17.082a23.848 23.848 0 0 0 5.454-1.31A8.967 8.967 0 0 1 18 9.75V9A6 6 0 0 0 6 9v.75a8.967 8.967 0 0 1-2.312 6.022c1.733.64 3.56 1.085 5.455 1.31m5.714 0a24.255 24.255 0 0 1-5.714 0m5.714 0a3 3 0 1 1-5.714 0"/>
@@ -73,18 +63,18 @@
                                 From: "transform opacity-100 scale-100"
                                 To: "transform opacity-0 scale-95"
                             -->
-{{--                            <div--}}
-{{--                                class="absolute right-0 z-10 mt-2 w-48 origin-top-right rounded-md bg-white py-1 ring-1 shadow-lg ring-black/5 focus:outline-hidden"--}}
-{{--                                role="menu" aria-orientation="vertical" aria-labelledby="user-menu-button"--}}
-{{--                                tabindex="-1">--}}
-{{--                                <!-- Active: "bg-gray-100 outline-hidden", Not Active: "" -->--}}
-{{--                                <a href="#" class="block px-4 py-2 text-sm text-gray-700" role="menuitem" tabindex="-1"--}}
-{{--                                   id="user-menu-item-0">Your Profile</a>--}}
-{{--                                <a href="#" class="block px-4 py-2 text-sm text-gray-700" role="menuitem" tabindex="-1"--}}
-{{--                                   id="user-menu-item-1">Settings</a>--}}
-{{--                                <a href="#" class="block px-4 py-2 text-sm text-gray-700" role="menuitem" tabindex="-1"--}}
-{{--                                   id="user-menu-item-2">Sign out</a>--}}
-{{--                            </div>--}}
+                            {{--                            <div--}}
+                            {{--                                class="absolute right-0 z-10 mt-2 w-48 origin-top-right rounded-md bg-white py-1 ring-1 shadow-lg ring-black/5 focus:outline-hidden"--}}
+                            {{--                                role="menu" aria-orientation="vertical" aria-labelledby="user-menu-button"--}}
+                            {{--                                tabindex="-1">--}}
+                            {{--                                <!-- Active: "bg-gray-100 outline-hidden", Not Active: "" -->--}}
+                            {{--                                <a href="#" class="block px-4 py-2 text-sm text-gray-700" role="menuitem" tabindex="-1"--}}
+                            {{--                                   id="user-menu-item-0">Your Profile</a>--}}
+                            {{--                                <a href="#" class="block px-4 py-2 text-sm text-gray-700" role="menuitem" tabindex="-1"--}}
+                            {{--                                   id="user-menu-item-1">Settings</a>--}}
+                            {{--                                <a href="#" class="block px-4 py-2 text-sm text-gray-700" role="menuitem" tabindex="-1"--}}
+                            {{--                                   id="user-menu-item-2">Sign out</a>--}}
+                            {{--                            </div>--}}
                         </div>
                     </div>
                 </div>
@@ -144,16 +134,16 @@
                         </svg>
                     </button>
                 </div>
-{{--                <div class="mt-3 space-y-1 px-2">--}}
-{{--                    <a href="#"--}}
-{{--                       class="block rounded-md px-3 py-2 text-base font-medium text-gray-400 hover:bg-gray-700 hover:text-white">Your--}}
-{{--                        Profile</a>--}}
-{{--                    <a href="#"--}}
-{{--                       class="block rounded-md px-3 py-2 text-base font-medium text-gray-400 hover:bg-gray-700 hover:text-white">Settings</a>--}}
-{{--                    <a href="#"--}}
-{{--                       class="block rounded-md px-3 py-2 text-base font-medium text-gray-400 hover:bg-gray-700 hover:text-white">Sign--}}
-{{--                        out</a>--}}
-{{--                </div>--}}
+                {{--                <div class="mt-3 space-y-1 px-2">--}}
+                {{--                    <a href="#"--}}
+                {{--                       class="block rounded-md px-3 py-2 text-base font-medium text-gray-400 hover:bg-gray-700 hover:text-white">Your--}}
+                {{--                        Profile</a>--}}
+                {{--                    <a href="#"--}}
+                {{--                       class="block rounded-md px-3 py-2 text-base font-medium text-gray-400 hover:bg-gray-700 hover:text-white">Settings</a>--}}
+                {{--                    <a href="#"--}}
+                {{--                       class="block rounded-md px-3 py-2 text-base font-medium text-gray-400 hover:bg-gray-700 hover:text-white">Sign--}}
+                {{--                        out</a>--}}
+                {{--                </div>--}}
             </div>
         </div>
     </nav>
